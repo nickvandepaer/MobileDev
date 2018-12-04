d0f359692e8d667b152a1de5389a0213f87bfc51
r0705232 <r0705232@student.thomasmore.be>
2018-12-04 13:09:23 +0100

    schermen knoppen fix

---
+++ app/src/main/java/be/thomasmore/flinkspreken/FinaalInitiaalActivity.java
@@ -0,0 +1,59 @@
+package be.thomasmore.flinkspreken;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class FinaalInitiaalActivity extends AppCompatActivity {
+
+    String frontingStopping;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finaal_initiaal);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        Bundle bundle = getIntent().getExtras();
+        frontingStopping = bundle.getString("FrontingStoppingKeuze");
+        toon(frontingStopping);
+    }
+
+    private void toon(String tekst)
+    {
+        Toast.makeText(getBaseContext(), tekst, Toast.LENGTH_SHORT).show();
+    }
+
+    public void Finaal_onClick(View v) {
+        Bundle bundle = new Bundle();
+        bundle.putString("FrontingStoppingKeuze", frontingStopping);
+        bundle.putString("FinaalInitiaalKeuze", "finaal");
+        Intent intent = new Intent(this, DoelklankActivity.class);
+        intent.putExtras(bundle);
+        startActivityForResult(intent, 1);  // 1 is requestCode
+    }
+
+    public void Initiaal_onClick(View v) {
+        Bundle bundle = new Bundle();
+        bundle.putString("FrontingStoppingKeuze", frontingStopping);
+        bundle.putString("FinaalInitiaalKeuze", "initiaal");
+        Intent intent = new Intent(this, DoelklankActivity.class);
+        intent.putExtras(bundle);
+        startActivityForResult(intent, 1);  // 1 is requestCode
+    }
+}
