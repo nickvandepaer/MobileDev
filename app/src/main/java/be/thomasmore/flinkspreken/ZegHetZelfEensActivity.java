b54a638eed81ec7026a316cc4c379f93af38397a
r0717250 <r0712750@thomasmore.student.be>
2018-12-11 14:43:51 +0100

    zeg het zelf eens

--- app/src/main/java/be/thomasmore/flinkspreken/ZegHetZelfEensActivity.java
+++ app/src/main/java/be/thomasmore/flinkspreken/ZegHetZelfEensActivity.java
@@ -1,6 +1,7 @@
 package be.thomasmore.flinkspreken;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,25 +11,67 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ZegHetZelfEensActivity extends AppCompatActivity {
 
     int KOLOM = 3;
     int RIJ = 3;
+    String frontingStopping;
+    String finaalInitiaal;
+    String doelKlank;
+    String minimalePaar;
+    private DatabaseHelper db;
+    public List<MinimalePaar> minimaleparen;
+    public List<MinimalePaar> filteredListMinimaleparen = new ArrayList<MinimalePaar>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zeg_het_zelf_eens);
+
+        Bundle bundle = getIntent().getExtras();
+        frontingStopping = bundle.getString("FrontingStoppingKeuze");
+        finaalInitiaal = bundle.getString("FinaalInitiaalKeuze");
+        doelKlank = bundle.getString("Doelklank");
+        minimalePaar = bundle.getString("MinimalePaar");
+
+        db = new DatabaseHelper(this);
+        minimaleparen = db.getMinimaleParen();
+
+        HaalDataOp();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         maakLayout();
 
     }
 
+    private void HaalDataOp()
+    {
+
+        //haalt alles op
+        for (int i=0; i<minimaleparen.size(); i++)
+        {
+            //controlleerd wat gelijk is aan wat we meegeven
+            if(minimaleparen.get(i).getDoelKlank().equals(doelKlank) && minimaleparen.get(i).getFinaalInitiaal().equals(finaalInitiaal)){
+                //aparte lijst
+                filteredListMinimaleparen.add(minimaleparen.get(i));
+            }
+        }
+    }
+
+    private void toon(String tekst)
+    {
+        Toast.makeText(getBaseContext(), tekst, Toast.LENGTH_SHORT).show();
+    }
+
     private void maakLayout()
     {
-        int k = 0;
+    int k = 0;
         LinearLayout mainLayout = (LinearLayout) findViewById(R.id.layout_main);
         for (int i = 0; i < RIJ; i++) {
             LinearLayout linearLayout = new LinearLayout(this);
@@ -45,7 +88,7 @@ public class ZegHetZelfEensActivity extends AppCompatActivity {
                 imageLayoutParams.leftMargin = 5;
                 imageLayoutParams.topMargin = 5;
                 imageView.setLayoutParams(imageLayoutParams);
-                imageView.setTag("oor" + k);
+                imageView.setTag("oor");
 
                 imageView.setOnClickListener(new View.OnClickListener() {
                     public  void onClick(View v)
@@ -69,6 +112,7 @@ public class ZegHetZelfEensActivity extends AppCompatActivity {
 
         Bundle bundle = new Bundle();
         bundle.putString("logo", imageView.getTag().toString());
+        bundle.putString("minimalepaar", minimalePaar);
 
         Intent intent = new Intent(this, ZegHetZelfEensSchermAcitivity.class);
         intent.putExtras(bundle);
--- app/src/main/java/be/thomasmore/flinkspreken/ZegHetZelfEensSchermAcitivity.java
+++ app/src/main/java/be/thomasmore/flinkspreken/ZegHetZelfEensSchermAcitivity.java
@@ -5,12 +5,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Display;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
 
 public class ZegHetZelfEensSchermAcitivity extends AppCompatActivity {
 
     String logo;
+    String woord;
+    String displayWoord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +28,46 @@ public class ZegHetZelfEensSchermAcitivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toonLogo();
+        KiesWoord();
 
     }
 
+    private void toon(String tekst)
+    {
+        Toast.makeText(getBaseContext(), tekst, Toast.LENGTH_SHORT).show();
+    }
 
     private void toonLogo() {
         Bundle bundle = getIntent().getExtras();
+
         logo = bundle.getString("logo");
 
         ImageView imageView = (ImageView) findViewById(R.id.logo);
         imageView.setImageResource(getResources().getIdentifier(logo,"drawable",getPackageName()));
+    }
+
+    private  void KiesWoord()
+    {
+        Bundle bundle = getIntent().getExtras();
+
+        woord = bundle.getString("minimalepaar");
+        String[] splitted = woord.split("-");
+
+        Random rand = new Random();
+
+        int random = rand.nextInt(10) + 1;
+
+        if(random > 5)
+        {
+            displayWoord = splitted[0];
+        }
+        else
+        {
+            displayWoord = splitted[1];
+        }
+
+        TextView textView = (TextView) findViewById(R.id.woord);
+        textView.setText(displayWoord);
     }
 
 }
