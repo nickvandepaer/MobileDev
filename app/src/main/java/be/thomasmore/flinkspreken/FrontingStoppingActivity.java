01aff10144957bb4f0424653bb6cb050cc937cbc
r0705232 <r0705232@student.thomasmore.be>
2018-12-12 15:37:44 +0100

    layout scherm 1+2

--- app/src/main/java/be/thomasmore/flinkspreken/FrontingStoppingActivity.java
+++ app/src/main/java/be/thomasmore/flinkspreken/FrontingStoppingActivity.java
@@ -1,9 +1,11 @@
 package be.thomasmore.flinkspreken;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -34,5 +36,22 @@ public class FrontingStoppingActivity extends AppCompatActivity {
         intent.putExtras(bundle);
         startActivityForResult(intent, 1);  // 1 is requestCode
     }
+
+    public void Vraagteken_onClick(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        String alert1 = "Fronting: ";
+        String alert2 = "Het kind spreekt een klank, die vanachter in de mond uitgesproken moet worden, vooraan in de mond uit. Bv: toe i.p.v. koe" ;
+        String alert3 = " " ;
+        String alert4 = "Stopping: ";
+        String alert5 = "Het kind ‘stopt’ een klank die je normaal gezien lang moet aanhouden. Bv: kat i.p.v. gat" ;
+        builder.setMessage(alert1 +"\n"+ alert2 +"\n"+ alert3 +"\n"+ alert4 +"\n"+ alert5).setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 
 }
