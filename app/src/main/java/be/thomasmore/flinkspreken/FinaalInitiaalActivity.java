61eb23722bb1459550f95f30028cd59c6a82d716
r0705232 <r0705232@student.thomasmore.be>
2018-12-12 15:47:27 +0100

    layout scherm 3

--- app/src/main/java/be/thomasmore/flinkspreken/FinaalInitiaalActivity.java
+++ app/src/main/java/be/thomasmore/flinkspreken/FinaalInitiaalActivity.java
@@ -1,9 +1,11 @@
 package be.thomasmore.flinkspreken;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -44,5 +46,21 @@ public class FinaalInitiaalActivity extends AppCompatActivity {
         super.onStart();
         Bundle bundle = getIntent().getExtras();
         frontingStopping = bundle.getString("FrontingStoppingKeuze");
+    }
+
+    public void Vraagteken_onClick(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        String alert1 = "Finaal: ";
+        String alert2 = "Achteraan het woord." ;
+        String alert3 = " " ;
+        String alert4 = "Initiaal: ";
+        String alert5 = "Vooraan het woord." ;
+        builder.setMessage(alert1 +"\n"+ alert2 +"\n"+ alert3 +"\n"+ alert4 +"\n"+ alert5).setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
